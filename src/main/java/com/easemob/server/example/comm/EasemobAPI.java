4a4c92adf4afc1f03122dd17fc3d0bdc583321d2
ChenLaiHong <1185630400@qq.com>
2018-02-08 19:50:50 +0800

    easemob

---
+++ src/main/java/com/easemob/server/example/comm/EasemobAPI.java
@@ -0,0 +1,10 @@
+package com.easemob.server.example.comm;
+
+import io.swagger.client.ApiException;
+
+/**
+ * Created by CHLaih on 2018/2/8.
+ */
+public interface EasemobAPI {
+    Object invokeEasemobAPI() throws ApiException;
+}
---
+++ src/main/java/com/easemob/server/example/comm/OrgInfo.java
@@ -0,0 +1,29 @@
+package com.easemob.server.example.comm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by CHLaih on 2018/2/8.
+ */
+public class OrgInfo {
+    public static String ORG_NAME;
+    public static String APP_NAME;
+    public static final Logger logger = LoggerFactory.getLogger(OrgInfo.class);
+
+    static {
+        InputStream inputStream = OrgInfo.class.getClassLoader().getResourceAsStream("config.properties");
+        Properties prop = new Properties();
+        try {
+            prop.load(inputStream);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        ORG_NAME = prop.getProperty("ORG_NAME");
+        APP_NAME = prop.getProperty("APP_NAME");
+    }
+}
